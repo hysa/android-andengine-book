679c5f4d91227684ad6839d770ad2782d9757782
hysa <hysa329@gmail.com>
2014-02-01 00:26:38 +0900

    p55/Chapter-2-5-2まで。

--- chp2/src/me/hysa/library/base/MainScene.java
+++ chp2/src/me/hysa/library/base/MainScene.java
@@ -25,6 +25,7 @@ public class MainScene extends Scene {
     // Sceneを管理するActivityのインスタンスを保持
     // アプリの場合のContextと同じように利用できる
     private BaseGameActivity baseActivity;
+    private ResourceUtil resourceUtil;
 
     public MainScene(BaseGameActivity baseActivity) {
         this.baseActivity = baseActivity;
@@ -32,23 +33,7 @@ public class MainScene extends Scene {
     }
 
     public void init() {
-        // 画像リソースが格納されている場所を指定
-        // BitmapTextureAtlasTextureRegionFactoryはここでしか使用しない
-        BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
-        // オブジェクトのサイズを指定。480、800が収まる2のべき乗。
-        BitmapTextureAtlas bta = new BitmapTextureAtlas(
-                baseActivity.getTextureManager(), 512, 1024, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
-        // 範囲をメモリ上に読み込み
-        baseActivity.getTextureManager().loadTexture(bta);
-        // メモリ上に読み込んだ範囲に画像を読み込み。座標は0,0。
-        ITextureRegion btr = BitmapTextureAtlasTextureRegionFactory.createFromAsset(
-                bta, baseActivity, "main_bg.png", 0, 0);
-
-        // Spriteをインスタンス化。座標は0, 0。
-        Sprite bg = new Sprite(0, 0, btr, baseActivity.getVertexBufferObjectManager());
-        // Spriteをアルファ値取り扱いを設定
-        bg.setBlendFunction(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
-        // 画面に配置
-        attachChild(bg);
+        resourceUtil = ResourceUtil.getInstance(baseActivity);
+        attachChild(resourceUtil.getSprite("main_bg.png"));
     }
 }
