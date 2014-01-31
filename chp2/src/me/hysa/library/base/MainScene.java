2c56bce06bad134ae083b9ac5caa39b4b3f0c239
hysa <hysa329@gmail.com>
2014-01-31 23:28:23 +0900

    p48/Chapter-2-5-1まで。

--- chp2/src/me/hysa/library/base/MainScene.java
+++ chp2/src/me/hysa/library/base/MainScene.java
@@ -6,7 +6,14 @@
 
 package me.hysa.library.base;
 
+import javax.microedition.khronos.opengles.GL10;
+
 import org.andengine.entity.scene.Scene;
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.opengl.texture.TextureOptions;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.andengine.opengl.texture.region.ITextureRegion;
 import org.andengine.ui.activity.BaseGameActivity;
 
 /**
@@ -25,7 +32,23 @@ public class MainScene extends Scene {
     }
 
     public void init() {
+        // 画像リソースが格納されている場所を指定
+        // BitmapTextureAtlasTextureRegionFactoryはここでしか使用しない
+        BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
+        // オブジェクトのサイズを指定。480、800が収まる2のべき乗。
+        BitmapTextureAtlas bta = new BitmapTextureAtlas(
+                baseActivity.getTextureManager(), 512, 1024, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+        // 範囲をメモリ上に読み込み
+        baseActivity.getTextureManager().loadTexture(bta);
+        // メモリ上に読み込んだ範囲に画像を読み込み。座標は0,0。
+        ITextureRegion btr = BitmapTextureAtlasTextureRegionFactory.createFromAsset(
+                bta, baseActivity, "main_bg.png", 0, 0);
 
+        // Spriteをインスタンス化。座標は0, 0。
+        Sprite bg = new Sprite(0, 0, btr, baseActivity.getVertexBufferObjectManager());
+        // Spriteをアルファ値取り扱いを設定
+        bg.setBlendFunction(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
+        // 画面に配置
+        attachChild(bg);
     }
-
 }
