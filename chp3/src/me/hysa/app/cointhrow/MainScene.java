df5b55a0370980db12e48db202fe6f48af1aeeb1
hysa <hysa329@gmail.com>
2014-02-13 22:26:15 +0900

    p.80。フリック操作の実装。

--- chp3/src/me/hysa/app/cointhrow/MainScene.java
+++ chp3/src/me/hysa/app/cointhrow/MainScene.java
@@ -6,16 +6,25 @@
 
 package me.hysa.app.cointhrow;
 
+import org.andengine.entity.scene.IOnSceneTouchListener;
+import org.andengine.entity.scene.Scene;
 import org.andengine.entity.sprite.AnimatedSprite;
+import org.andengine.input.touch.TouchEvent;
 
+import android.util.Log;
 import android.view.KeyEvent;
 
 /**
  * @author hysa
  *
  */
-public class MainScene extends KeyListenScene {
+public class MainScene extends KeyListenScene implements IOnSceneTouchListener {
     private AnimatedSprite mCoin;
+
+    // ドラッグ開始位置
+    private float[] mTouchStartPoint;
+    // コインが飛び出す角度
+    private double mFlyAngle;
 
     public MainScene(MultiSceneActivity baseActivity) {
         super(baseActivity);
@@ -24,6 +33,9 @@ public class MainScene extends KeyListenScene {
 
     public void init() {
         attachChild(getBaseActivity().getResourceUtil().getSprite("main_bg.png"));
+
+        mTouchStartPoint = new float[2];
+        setOnSceneTouchListener(this);
 
         setNewCoin();
     }
@@ -56,6 +68,73 @@ public class MainScene extends KeyListenScene {
     @Override
     public boolean dispatchKeyEvent(KeyEvent e) {
         return false;
+    }
+
+    /**
+     * タッチイベントが発生したら呼ばれる
+     * @see org.andengine.entity.scene.IOnSceneTouchListener#onSceneTouchEvent(org.andengine.entity.scene.Scene, org.andengine.input.touch.TouchEvent)
+     */
+    @Override
+    public boolean onSceneTouchEvent(Scene pScene, TouchEvent pSceneTouchEvent) {
+        float x = pSceneTouchEvent.getX();
+        float y = pSceneTouchEvent.getY();
+
+        // 指が触れた瞬間のイベント
+        // タッチの座標がコイン上であるかチェック
+        if (pSceneTouchEvent.getAction() == TouchEvent.ACTION_DOWN
+                && (x > mCoin.getX() && x < mCoin.getX() + mCoin.getWidth())
+                && (y > mCoin.getY() && y < mCoin.getY() + mCoin.getHeight())) {
+
+            // 開始点を記録
+            mTouchStartPoint[0] = x;
+            mTouchStartPoint[1] = y;
+
+        // 指が触れた時、何らかの原因でタッチ処理が中断した場合のイベント
+        } else if (pSceneTouchEvent.getAction() == TouchEvent.ACTION_UP
+                || pSceneTouchEvent.getAction() == TouchEvent.ACTION_CANCEL) {
+
+            // 終点を記録
+            float[] touchEndPoint = new float[2];
+            touchEndPoint[0] = x;
+            touchEndPoint[1] = y;
+
+            // フリックの距離が短すぎるときにはフリックと判定しない
+            if (touchEndPoint[0] - mTouchStartPoint[0] < 50
+                    && touchEndPoint[0] - mTouchStartPoint[0] > -50
+                    && touchEndPoint[1] - mTouchStartPoint[1] < 50
+                    && touchEndPoint[1] - mTouchStartPoint[1] > -50) {
+                return true;
+            }
+
+            // フリックの角度を求める
+            mFlyAngle = getAngleByTwoPosition(mTouchStartPoint, touchEndPoint);
+            // 下から上へのフリックを0°に調整
+            mFlyAngle -= 180;
+            // 出力
+            Log.d("ae", "angle ::: " + mFlyAngle);
+        }
+
+
+        return true;
+    }
+
+    /**
+     * ２点間の角度を求める公式
+     *
+     * @param start 開始
+     * @param end 終了
+     * @return 角度
+     */
+    private double getAngleByTwoPosition(float[] start, float[] end) {
+
+        double result = 0;
+
+        float xDistance = end[0] - start[0];
+        float yDistance = end[1] - start[1];
+
+        result = Math.atan2((double) yDistance, (double) xDistance) * 180 / Math.PI;
+        result += 270;
+        return result;
     }
 }
 
